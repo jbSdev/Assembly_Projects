6f6031c83e1cb374347ae7d19d6870cec3aa1274
jbSdev <antoni.jesien@gmail.com>
2025-05-13 01:07:54 +0200

    Finding markers
    Working finding all markers (some bugs to check)
      listing their position, arm length and width.
    
    ToDo:
     - Eliminating multiple markers from one that was previously found

--- RISC-V_Project/ajesien_riscv_project.asm
+++ RISC-V_Project/ajesien_riscv_project.asm
@@ -8,7 +8,10 @@
 .eqv	Img_padding	24
 
 .eqv	Img_desc_size	28
+	# proper image
 #.eqv	IMGMAXSIZE	230400	# 240 * 320 * 3
+#.eqv	PixelCount	76800
+	# 30x20
 .eqv	IMGMAXSIZE	1840
 .eqv	PixelCount	600
 .eqv	Bmp_marker	0x4D42
@@ -58,6 +61,7 @@
 	exitText:	.asciz "\nExiting program..."
 	blank:		.asciz " "
 	endl:		.asciz "\n"
+	marker:		.asciz "\nmarker: "
 	
 
 .text
@@ -225,7 +229,8 @@ printData:
 #	prints to console all found black pixel coords (0, 0) - left top#
 # Internal values:							#
 #	t4  - padding to pixel						#
-#	s3  - marker arm length						#
+#	s2  - marker arm length						#
+#	s3  - image height						#
 #	s4  - image bits per line					#
 #	s5  - image width						#
 #	s6  - row padding						#
@@ -238,22 +243,23 @@ findMarkers:
 	mv	s11, ra
 	
 	# load initial values
-	xor	s8, s8, s8
-	xor	s9, s9, s9
-	xor	s10, s10, s10
+	lhu	s3, Img_height(a0)
 	lhu	s4, Img_bpl(a0)
 	lhu	s5, Img_width(a0)
 	lhu	s6, Img_padding(a0)
 	li	s7, PixelCount
+	xor	s8, s8, s8
+	xor	s9, s9, s9
+	xor	s10, s10, s10
 	
 	lw	t4, Img_imgdata(a0)
-	lhu	t0, Img_height(a0)
-	addi	t0, t0, -1
+	addi	t0, s3, -1
 	mul	t0, t0, s4
 	add	t4, t4, t0	# t4 points to (0, 0) - left top
 
 	
 findLoop:
+	xor	s1, s1, s1
 	beq	s10, s7, findEnd	# EOF
 	#jal	getPixelValue
 
@@ -270,21 +276,43 @@ findLoop:
 	# check length of arm to right
 	mv	t5, t4		# origin pixel
 	mv	t6, s8		# x coordinate
+	mv	t2, s8
 	jal	goRight
+	mv	s2, s1		# save arm length
 	
+	# check length of arm down
 	mv	t5, t4
 	mv	t6, s9		# y coordinate
-	lhu	t3, Img_height(a0)
+	mv	t3, s9
 	jal	goDown
 	
-	# s1 - length of right marker's arm
-	# s2 - length of left marker's arm
-	#addi	s1, s1, -1
-	#beqz	s1, nextPixel	# if it is a single pixel
-	#addi	s1, s1, 1
+	bne	s1, s2, nextPixel	# arms not equal in length
 	
-	beq	s1, s2, printCoords
+	addi	s2, s2, -1
+	beqz	s2, nextPixel	# if found a single pixel
+	addi	s2, s2, 1
+	
+	#li	a7, Sys_PrintString
+	#la	a0, marker
+	#ecall
+	#jal	pp
+	
+	# check diagonal pixels - arm may have width
+	mv	t5, t4
+	addi	t5, t5, 3
+	sub	t5, t5, s4	# t5 - offset of the first diagonal pixel from the base of marker
+	mv	t2, s8
+	addi	t2, t2, 1
+	mv	t3, s9
+	addi	t3, t3, 1
+	li	a6, 1		# width
+	
+	#mv	a5, pc, 4
+	jal	a5, goDiag
+	
+	jal	printCoords
 	j	nextPixel
+	
 	
 goRight:
 	beq	t6, s5, foundLengthRight
@@ -302,11 +330,12 @@ goRight:
 	
 	
 foundLengthRight:
-	sub	s1, t6, s8	# marker arm length
+	sub	s1, t6, t2	# right arm length
 	ret
 
 goDown:
-	beq	t6, t3, foundLengthDown
+	beq	t6, s3, foundLengthDown
+	
 	lbu	t1, 0(t5)	# Blue
 	bnez	t1, foundLengthDown
 	lbu	t1, 1(t5)	# Green
@@ -314,14 +343,107 @@ goDown:
 	lbu	t1, 2(t5)	# Red
 	bnez	t1, foundLengthDown
 	
-	add	t5, t5, s4
+	sub	t5, t5, s4
 	addi	t6, t6, 1
 	j	goDown
 	
 foundLengthDown:
-	sub	s2, t6, s9
+	sub	s1, t6, t3	# marker down arm length
 	ret
 
+#---------------------------------------#
+# 	     goDiag			#
+# Data:					#
+#	t5 - offset of first diag px	#
+#	t2 - current x			#
+#	t3 - current y			#
+#	s2 - base arm length		#
+#---------------------------------------#
+goDiag:
+	#mv	a5, ra
+	# if border is met, there is no possibility for that marker to be valid
+	beq	t2, s3, nextPixel
+	beq	t3, s5, nextPixel
+	#jal	pp
+	
+	lbu	t1, 0(t5)	# Blue
+	bnez	t1, foundDiag
+	lbu	t1, 1(t5)	# Green
+	bnez	t1, foundDiag
+	lbu	t1, 2(t5)	# Red
+	bnez	t1, foundDiag
+	
+	# the pixel is black
+	mv	t0, t5		# save temp origin offset
+	mv	t6, t2		# x coordinate
+	jal	goRight
+	mv	s0, s1		# save found length
+	
+	mv	t5, t0
+	mv	t6, t3		# y coordinate
+	jal	goDown
+	
+	mv	t5, t0
+	
+	sub	t0, t2, s8	# width offset
+	
+	# s0 and s1 contain found lengths
+	bne	s0, s1, nextPixel	# internal arms not equal
+	
+	#li	a7, Sys_PrintInt
+	#mv	a0, t0
+	#ecall
+	#mv	a0, s0
+	#ecall
+	#mv	a0, s2
+	#ecall
+	#li	a7, Sys_PrintString
+	#la	a0, endl
+	#ecall
+	
+	add	s0, s0, t0		# internal arm should be shorter by current width offset than the base ones
+	bne	s0, s2, nextPixel	# arms not equal to base ones
+	sub	s0, s0, t0
+	
+	# move diagonally
+	addi	t2, t2, 1	# x++
+	addi	t3, t3, 1	# y++
+	# border checked at start
+	addi	t5, t5, 3
+	sub	t5, t5, s4	# move offset diagonally
+	addi	a6, a6, 1
+	j	goDiag
+
+foundDiag:
+	#sub	t0, t2, s8	# width offset
+	# x in s8
+	# y in s9
+	# length in s2
+	# width in a6
+	mv	ra, a5
+	ret
+	
+	
+pp:
+	mv	t6, a0
+	li	a7, Sys_PrintInt
+	mv	a0, s8		# x
+	ecall
+	li	a7, Sys_PrintString
+	la	a0, blank	# ' '
+	ecall
+	li	a7, Sys_PrintInt
+	mv	a0, s9		# y
+	ecall
+	li	a7, Sys_PrintString
+	la	a0, blank	# ' '
+	ecall
+	li	a7, Sys_PrintString
+	la	a0, endl	# '\n'
+	ecall
+	mv	a0, t6
+	ret
+	
 #-------------------------------#
 # 	  Print Coords		#
 # Arguments:			#
@@ -335,25 +457,26 @@ printCoords:
 	li	a7, Sys_PrintString
 	la	a0, endl	# '\n'
 	ecall
-	
 	li	a7, Sys_PrintInt
 	mv	a0, s8		# x
 	ecall
-	
 	li	a7, Sys_PrintString
 	la	a0, blank	# ' '
 	ecall
-	
 	li	a7, Sys_PrintInt
 	mv	a0, s9		# y
 	ecall
-	
 	li	a7, Sys_PrintString
 	la	a0, blank	# ' '
 	ecall
-	
 	li	a7, Sys_PrintInt
-	mv	a0, s1		# length
+	mv	a0, s2		# length
+	ecall
+	li	a7, Sys_PrintString
+	la	a0, blank	# ' '
+	ecall
+	li	a7, Sys_PrintInt
+	mv	a0, a6		# width
 	ecall
 	
 	mv	a0, t6
@@ -362,7 +485,7 @@ nextPixel:
 	addi	s8, s8, 1
 	addi	s10, s10, 1
 	addi	t4, t4, 3	# next pixel
-	bne 	s8, s5, findLoop
+	bne 	s8, s5, findLoop# end of line
 	
 	xor	s8, s8, s8
 	addi	s9, s9, 1
